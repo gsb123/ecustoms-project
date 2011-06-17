9751bfd5e3ab58f626b4148fcb56d7bd59177f03
vuongfat <vuongfat@gmail.com>
2011-06-17 16:16:38 +0000

    Deleted mot so method ko dung

--- ECustoms.DAL/DeclarationDAL.cs
+++ ECustoms.DAL/DeclarationDAL.cs
@@ -21,76 +21,6 @@ namespace ECustoms.DAL
             _dbConnection = new DbConnection(_connectionString);
         }
 
-        /// <summary>
-        /// Insert new Declaration
-        /// </summary>
-        /// <param name="declarationInfo">DeclarationInfo object</param>
-        /// <returns>new id</returns>
-        public int Insert(DeclarationInfo declarationInfo)
-        {
-
-            var result = -1;
-            try
-            {                
-                var parameters = new SqlParameter[18];
-
-                parameters[0] = new SqlParameter("@Number", declarationInfo.Number);
-                parameters[1] = new SqlParameter("@ProductName", declarationInfo.ProductName);
-                parameters[2] = new SqlParameter("@CompanyName", declarationInfo.CompanyName);
-                parameters[3] = new SqlParameter("@ProductAmount", declarationInfo.ProductAmount);
-                parameters[4] = new SqlParameter("@Unit", declarationInfo.Unit);
-                parameters[5] = new SqlParameter("@CreatedByID", declarationInfo.CreatedByID);
-                parameters[6] = new SqlParameter("@ModifiedByID", declarationInfo.ModifiedByID);
-                parameters[7] = new SqlParameter("@ModifiedDate", declarationInfo.ModifiedDate);
-                parameters[8] = new SqlParameter("@CreatedDate", DateTime.Now);
-                parameters[9] = new SqlParameter("@HasDeclaration", Convert.ToInt32(declarationInfo.HasDeclaration));
-                parameters[10] = new SqlParameter("@ImportNumber", declarationInfo.ImportNumber);
-                parameters[11] = new SqlParameter("@ImportProductName", declarationInfo.ImportProductName);
-                parameters[12] = new SqlParameter("@ImportCompanyName", declarationInfo.ImportCompanyName);
-                parameters[13] = new SqlParameter("@ImportProductAmount", declarationInfo.ImportProductAmount);
-                parameters[14] = new SqlParameter("@ImportUnit", declarationInfo.ImportUnit);
-                parameters[15] = new SqlParameter("@ImportHasDeclaration", Convert.ToInt32(declarationInfo.ImportHasDeclaration));
-
-                // Phuong Added
-                parameters[16] = new SqlParameter("@ImportType", declarationInfo.ImportType);
-                parameters[17] = new SqlParameter("@ExportType", declarationInfo.ExportType);
-             
-                
-
-                result = Convert.ToInt32( _dbConnection.ExecuteScalar(ConstantInfo.SP_INSERTDECLARATION, parameters));                
-                return result;
-            }
-            catch (Exception)
-            {
-                    
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// Get DeclerationInfo
-        /// </summary>
-        /// <returns></returns>
-        public List<DeclarationInfo> Select()
-        {
-            var result = new List<DeclarationInfo>();
-            DeclarationInfo declerationInfo;
-            try
-            {
-                var dataTable = _dbConnection.ExecuteSelectQuery(ConstantInfo.SP_SELECTDECLERATION);
-                foreach (DataRow dr in dataTable.Rows)
-                {
-                    declerationInfo = new DeclarationInfo();
-                    declerationInfo.CreateFrom(dr);
-                    result.Add(declerationInfo);
-                }
-                return result;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
 
         /// <summary>
         /// Delete Decleration by ID
@@ -115,72 +45,8 @@ namespace ECustoms.DAL
             }
         }
 
-        /// <summary>
-        /// Update Decleration by ID
-        /// </summary>
-        /// <param name="declerationID">DeclerationID</param>
-        /// <returns>Number of rows are effected</returns>
-        public int UpdateByID(DeclarationInfo declerationInfo)
-        {
-            var result = -1;
-            try
-            {
-                var parameters = new SqlParameter[17];
-                parameters[0] = new SqlParameter("@DeclerationID", declerationInfo.DeclarationID);
-                parameters[1] = new SqlParameter("@Number", declerationInfo.Number);
-                parameters[2] = new SqlParameter("@ProductName", declerationInfo.ProductName);
-                parameters[3] = new SqlParameter("@CompanyName", declerationInfo.CompanyName);
-                parameters[4] = new SqlParameter("@ProductAmount", declerationInfo.ProductAmount);
-                parameters[5] = new SqlParameter("@Unit", declerationInfo.Unit);                
-                parameters[6] = new SqlParameter("@ModifiedByID", declerationInfo.ModifiedByID);
-                parameters[7] = new SqlParameter("@ModifiedDate", declerationInfo.ModifiedDate);
-                parameters[8] = new SqlParameter("@HasDeclaration", Convert.ToInt32(declerationInfo.HasDeclaration));
+       
 
-                parameters[9] = new SqlParameter("@ImportNumber", declerationInfo.ImportNumber);
-                parameters[10] = new SqlParameter("@ImportProductName", declerationInfo.ImportProductName);
-                parameters[11] = new SqlParameter("@ImportCompanyName", declerationInfo.ImportCompanyName);
-                parameters[12] = new SqlParameter("@ImportProductAmount", declerationInfo.ImportProductAmount);
-                parameters[13] = new SqlParameter("@ImportUnit", declerationInfo.ImportUnit);
-                parameters[14] = new SqlParameter("@ImportHasDeclaration", Convert.ToInt32(declerationInfo.ImportHasDeclaration));
-
-                // Phuong Added
-                parameters[15] = new SqlParameter("@ImportType", declerationInfo.ImportType);
-                parameters[16] = new SqlParameter("@ExportType", declerationInfo.ExportType);
-             
-
-                result = Convert.ToInt32(_dbConnection.ExecuteScalar(ConstantInfo.SP_UPDATEDECLARATION, parameters));
-                return result;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// Get Declaration by ID
-        /// </summary>
-        /// <param name="declarationID">DeclarationID</param>
-        /// <returns>DeclarationInfo object</returns>
-        public DeclarationInfo SelectByID(int declarationID)
-        {
-            var declerationInfo = new DeclarationInfo();
-            try
-            {
-                var parameters = new SqlParameter[1];
-                parameters[0] = new SqlParameter("@DeclarationID", declarationID);
-
-                var dataTable = _dbConnection.ExecuteSelectQuery(ConstantInfo.SP_SELECTDECLERATIONBYID, parameters);
-                if (dataTable.Rows.Count == 1)
-                {
-                    declerationInfo.CreateFrom(dataTable.Rows[0]);   
-                }
-                return declerationInfo;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
+       
     }
 }
