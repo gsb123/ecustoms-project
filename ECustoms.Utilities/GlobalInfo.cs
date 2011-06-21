3a2f7a84ed8277c0b1d424b9ac47cf33befb7a8b
vuongfat <vuongfat@gmail.com>
2011-06-21 15:36:44 +0000

    Add GlobalInfo

---
+++ ECustoms.Utilities/GlobalInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECustoms.Utilities
+{
+    public static class GlobalInfo
+    {
+        public static bool IsDebug 
+        {
+            get { return Convert.ToBoolean(Convert.ToInt32(System.Configuration.ConfigurationSettings.AppSettings["IsDebugMode"].ToString())); }
+        }
+    }
+}
